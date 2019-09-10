085ee8b589a0cf85acb8202abcec27e73d1b10b0
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-09-10 15:12:49 -0700

    Testing new branch

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,7 @@
 const knex = require("knex");
 const knexFile = requires("../knexfile.js");
 const knexConfig = knexFile.development;
+//
 
-module.exports = knex(knexConfig);
+
+module.exports = knex(knexConfig);git 
